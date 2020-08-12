400813b16036e307e7c2a321c5049b0ab3808a03
Gabriel Costa <55096273+Promestein@users.noreply.github.com>
2020-08-12 17:52:15 -0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/* Add stylesheet in theme */
+function TemaCurso_resources(){
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'TemaCurso_resources');
+
+
+wp_register_script('jquery', get_template_directory_uri(). '/js/jquery.min.js');
+wp_enqueue_script('jquery');
+
+wp_register_script('jquery-dropotron', get_template_directory_uri(). '/js/jquery.dropotron.min.js');
+wp_enqueue_script('jquery-dropotron');
+
+wp_register_script('skel', get_template_directory_uri(). '/js/skel.min.js');
+wp_enqueue_script('skel');
+
+wp_register_script('skel-layers', get_template_directory_uri(). '/js/skel-layers.min.js');
+wp_enqueue_script('skel-layers');
+
+wp_register_script('init', get_template_directory_uri(). '/js/init.js');
+wp_enqueue_script('init');
+?>
